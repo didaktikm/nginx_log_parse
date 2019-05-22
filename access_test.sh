d6068059f48e8a7512c8aa4bf049e3f675c914ba
Pavel <slimjim@slimjim.ru>
2019-05-22 17:12:57 +0300

    no message

--- access_test.sh
+++ access_test.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 
+# author      Victor Magarlamov
+# email       victor.magarlamov@gmail.com
+# homepage    https://github.com/victor-magarlamov/nginx_log_analyzer
+# description analysis of access logs for a given day
+# version     0.0.1
+
+# ======================================================================
+# DAY - day for which you want to display a report
+# ======================================================================
+
+# DAY='19/Apr/2016'
+
+# ======================================================================
+# Variables and Helpers 
+# ======================================================================
+
+clear    # Очистка экрана
+
 FILES='access.log'
+
+LCYAN='\033[1;36m'     #  ${LCYAN}
+YELLOW='\033[0;33m'     #  ${YELLOW}
 
 COUNT_PER_SEC='count_per_sec.tmp'
 COUNT_BY_CODE='count_by_code.tmp'
 REQUEST_BY_FREQUENCY='request_by_frequency.tmp'
 REFERER_BY_FREQUENCY='referer_by_frequency.tmp'
-
-  find . -name '*.tmp' -type f -delete
   
   for file in $FILES; do
     awk '($4$5)' $file >> total.tmp
@@ -29,7 +48,7 @@ REFERER_BY_FREQUENCY='referer_by_frequency.tmp'
        }' total.tmp | sort | uniq -c | sort -rn -o $REFERER_BY_FREQUENCY
   wait
 
-      echo "Average of requests per second"
+      echo -en "${LYELLOW} Average of requests per second"
       awk '{s += $1} END {print s/NR}' $COUNT_PER_SEC 
    
       echo "Max of requests per second"
@@ -39,4 +58,8 @@ REFERER_BY_FREQUENCY='referer_by_frequency.tmp'
       awk '{print $2, $1}' $COUNT_BY_CODE
   
       echo "Top of Requests"
-      awk 'FNR <= 5 {print $1, $2}' $REQUEST_BY_FREQUENCY
+      awk -v y=$YELLOW 'FNR <= 5 {print $1, $2}' $REQUEST_BY_FREQUENCY
+	  
+	  tput sgr0
+	  
+	  rm -rf *.tmp
