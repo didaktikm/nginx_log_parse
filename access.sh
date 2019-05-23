f3e4cf53aef2eb625476d6dc8fc9f1b714f0954d
Pavel <slimjim@slimjim.ru>
2019-05-23 10:14:30 +0300

    no message

--- access.sh
+++ access.sh
@@ -23,14 +23,8 @@ REFERER_BY_FREQUENCY='referer_by_frequency.tmp'
            print substr($6$7, 0, ind)
          else
            print $6$7
-       }' total.tmp | sort | uniq -c | sort -rn -o $REQUEST_BY_FREQUENCY &
-  awk '{ ind = match($11, /\w\//)
-         if (ind > 0) 
-           print substr($11, 0, ind)
-         else
-           print $11
-       }' total.tmp | sort | uniq -c | sort -rn -o $REFERER_BY_FREQUENCY
-
+       }' total.tmp | sort | uniq -c | sort -rn -o $REQUEST_BY_FREQUENCY
+  
       echo -e "${LCYAN}Среднее количество запросов в секунду ${NORMAL}"
       awk '{s += $1} END {print "\033[0;33m" s/NR}' $COUNT_PER_SEC
    
@@ -41,7 +35,7 @@ REFERER_BY_FREQUENCY='referer_by_frequency.tmp'
       awk '{print "\033[0;33m" $2, $1}' $COUNT_BY_CODE
   
       echo -e "${LCYAN}Топ запросов ${NORMAL}"
-      awk 'FNR <= 5 {print  "\033[0;33m" $1, $2}' $REQUEST_BY_FREQUENCY
+      awk 'FNR <= 5 {print $1, $2}' $REQUEST_BY_FREQUENCY
 	  
 	  tput sgr0
 	  
