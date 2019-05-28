89c6e045ece6f57e400982e2556865b31b789766
Pavel <slimjim@slimjim.ru>
2019-05-28 10:43:24 +0300

    no message

--- access.sh
+++ access.sh
@@ -3,7 +3,7 @@
 clear   
 
 FILES='access.log'
-LRUN=$(ls -l --time-style=full-iso total.tmp | cut -c 45-52)
+LRUN=$(cat prev)
 
 LCYAN='\033[1;36m'     #  ${LCYAN}
 NORMAL='\033[0m'		# ${NORMAL}
@@ -11,12 +11,11 @@ NORMAL='\033[0m'		# ${NORMAL}
 COUNT_PER_SEC='count_per_sec.tmp'
 COUNT_BY_CODE='count_by_code.tmp'
 REQUEST_BY_FREQUENCY='request_by_frequency.tmp'
-REFERER_BY_FREQUENCY='referer_by_frequency.tmp'
   
-  for file in $FILES; do
-		awk '($4$5)' $file >> total.tmp
-		done
-		grep -F "15:55" total.tmp >> test.log
+  # for file in $FILES; do
+		tail -n +$LRUN $FILES | awk '($4$5)' >> total.tmp
+		# done
+		# grep -F "15:55" total.tmp >> test.log
 	
   awk '{print $4$5}' total.tmp | sort | uniq -c | sort -rn -o $COUNT_PER_SEC &
   awk '{print $9}' total.tmp | sort | uniq -c | sort -rn -o $COUNT_BY_CODE &
@@ -41,5 +40,14 @@ REFERER_BY_FREQUENCY='referer_by_frequency.tmp'
 	  
 	  tput sgr0
 	  
-	  echo "$LRUN"  
+	  wc -l access.log | awk '{print $1}' > prev
+	  
+	  CPS=$(awk '{s += $1} END {print "\033[0;33m" s/NR}' $COUNT_PER_SEC)
+	  MPS=$(awk 'NR == 1 {print "\033[0;33m" $1}' $COUNT_PER_SEC)
+	  CBC=$(awk '{print "\033[0;33m" $2, $1}' $COUNT_BY_CODE)
+	  RBF=$(awk 'FNR <= 5 {print "\033[0;33m" $1, $2}' $REQUEST_BY_FREQUENCY)
+	  
+	  
+	  echo -e "Среднее количество запросов в секунду:$CPS \n Максимальное количество запросов в секунду:$MPS\n Коды ответа сервера:$CBC\n Топ запросов:$RBF\n" | mail -s "Nginx logs" user@mail.com
+	  # echo "$LRUN"  
 	  # rm -rf *.tmp
